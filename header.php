cf58d6b4f222635c1fe3b0d96db6500b18206501
Aaron Snowberger <jekkilekki@gmail.com>
2016-03-01 08:05:07 +0900

    Fix some minor CSS including main title box

--- header.php
+++ header.php
@@ -157,26 +157,19 @@
                     <div class="site-header-img small-12 columns" style="background-image: url(<?php header_image(); ?>)"></div><!-- .site-header-image -->
                         
                         <?php if ( !is_single() && !is_page() ) : ?>
-                        <div class="site-main-title-box">
-                            <hgroup>
-                            
-                            <?php
-                            if ( is_front_page() && is_home() ) : ?>
-                                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                                    <?php
-                                    $description = get_bloginfo( 'description', 'display' );
-                                    if ( $description || is_customize_preview() ) : ?>
-                                        <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-                                    <?php
-                                    endif; ?>
-                            <?php 
-                            else : ?>
-                                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-                            <?php
-                            endif; ?>
-
-                            </hgroup>
-                        </div><!-- .site-main-title-box -->
+                        <div class="site-main-title-container">
+                            <div class="site-main-title-box">
+                                <hgroup>
+                                        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                                        <?php
+                                        $description = get_bloginfo( 'description', 'display' );
+                                        if ( $description || is_customize_preview() ) : ?>
+                                            <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                                        <?php
+                                        endif; ?>
+                                </hgroup>
+                            </div><!-- .site-main-title-box -->
+                        </div><!-- .site-main-title-container -->
                         <?php endif; ?>
                        
                 <?php elseif ( has_post_thumbnail() ) : ?>
